e84cc577985e76905b7977c9294e70238cbf5ebe
Vinicius Lopes <65102990+ViniciusLopesN@users.noreply.github.com>
2025-03-06 17:35:31 -0300

    finished dice roller

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputForm = document.getElementById('inputNumber');
+const button = document.getElementById('submitBtn');
+const includeText = document.getElementById('include-text');
+const diceDisplay = document.getElementById('dice-display');
+const diceValues = [];
+
+
+function rollDice(instances) {
+  for (let i = 0; i < instances; i++) {
+   let diceRoll = Math.floor(Math.random() * 6) + 1;
+   diceValues.push(diceRoll);
+   includeText.textContent = diceValues;
+  const diceImg = document.createElement('img');
+   diceImg.src = `./images/${diceRoll}.png`;
+   diceDisplay.appendChild(diceImg);
+  }
+}
+
+button.addEventListener("click", () => {
+  diceValues.length = 0;
+  diceDisplay.innerHTML = '';
+  let numberOfDice = inputForm.value;
+  if (numberOfDice > 100) {
+   diceDisplay.textContent = 'Too many dice, please choose 100 or less';
+  }
+  else{
+  rollDice(numberOfDice);
+  }
+})
